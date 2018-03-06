c80f2fcd6217a56e1834d5aeef38538e0e4d14e5
andreyp1987 <sumyrieltor777@gmail.com>
2018-03-07 00:55:58 +0200

    v.2

--- js/module-3.js
+++ js/module-3.js
@@ -17,14 +17,14 @@ const thirdLineKeybord = letters.slice(letters.indexOf('z'), letters.indexOf('/'
 const keyboard = [firstLineKeybord, secondLineKeybord, thirdLineKeybord];
 console.log (keyboard);
 
-const hello = (keyboard[1][5]) + (keyboard[0][2]) + (keyboard[1][8]) + (keyboard[1][8]) + (keyboard[0][8]); 
+const hello = keyboard[1][5] + keyboard[0][2] + keyboard[1][8] + keyboard[1][8] + keyboard[0][8]; 
 console.log(hello);
 
-const javascript = (keyboard[1][6]) + (keyboard[1][0]) + (keyboard[2][3]) + (keyboard[1][0]) + 
-(keyboard[1][1]) + (keyboard[2][2]) + (keyboard[0][3])  + (keyboard[0][7]) + (keyboard[0][9]) + 
-(keyboard[0][4]); 
+const javascript = keyboard[1][6] + keyboard[1][0] + keyboard[2][3] + keyboard[1][0] + 
+keyboard[1][1] + keyboard[2][2] + keyboard[0][3] + keyboard[0][7] + keyboard[0][9] + 
+keyboard[0][4]; 
 console.log(javascript);
 
-const trainer = (keyboard[0][4]) + (keyboard[0][3]) + (keyboard[1][0]) + (keyboard[0][7]) + 
-(keyboard[2][5]) + (keyboard[0][2]) + (keyboard[0][3]); 
+const trainer = keyboard[0][4] + keyboard[0][3] + keyboard[1][0] + keyboard[0][7] + 
+keyboard[2][5] + keyboard[0][2] + keyboard[0][3]; 
 console.log(trainer);
